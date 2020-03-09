7e367454ef505f6347b5945b951fbf5698ca638f
anjar <anjar.1912@students.amikom.ac.id>
2020-03-09 17:04:13 +0700

    menambahkan fungsi penambahan,pengurangan,perkalian,pembagian

---
+++ calculator.cs
@@ -0,0 +1,45 @@
+
+    class Calculator
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "APLIKASI KALKULATOR";
+            //int a = 9;
+            //int b = 12;
+
+            Console.Write("Inputkan nilai a =");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("Inputkan nilai b =");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+
+
+            Console.WriteLine("Hasil Penambahan  " + a + " + " + b + " = " + penambahan(a, b));
+            Console.WriteLine("Hasil Pengurangan  {0} - {1} = {2}", a, b, pengurangan(a, b));
+            Console.WriteLine("Hasil Perkalian {0} * {1} = {2}", a, b, perkalian(a, b));
+            Console.WriteLine("Hasil Pembagian {0} / {1} = {2}", a, b, pembagian(a, b));
+
+            Console.WriteLine("\nTekan sembarang key untuk keluar");
+            Console.ReadKey();
+        }
+
+        static int penambahan(int a, int b)
+        {
+            return a + b;
+        }
+        static int pengurangan(int a, int b)
+        {
+            return a - b;
+        }
+        static int perkalian(int a, int b)
+        {
+            return a * b;
+        }
+        static int pembagian(int a, int b)
+        {
+            return a / b;
+        }
+
+    }
